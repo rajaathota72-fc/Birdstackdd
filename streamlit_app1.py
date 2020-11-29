3510eda554c744d8bb5dfdcf94310e2c3f3efe6a
rajaathota72 <44434323+rajaathota72@users.noreply.github.com>
2020-11-29 19:35:08 +0100

    commit5

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -13,13 +13,54 @@ name = st.sidebar.text_input("Name with Salutation","Type here")
 role = st.sidebar.text_input("Role / Designation","Type here")
 organisation = st.sidebar.text_input("Name of your Organisation","Type here")
 st.sidebar.success("Select task to automate")
-dropdown = st.sidebar.selectbox("Drop down to select",("Sales invoice Processing","Tax Automation", "Mail Automation"))
-if st.sidebar.checkbox("Get Analysis Report"):
-    st.success("Hello Decision Maker, here is your RPA report")
-    st.image("DMimages/salesorder.png", width=700)
-    if st.button("Pay Now & Get Full Report"):
-        link = '[Click here to Navigate](https://rzp.io/l/0ne27uKX)'
-        st.markdown(link, unsafe_allow_html=True)
-    if st.button("Interested in Custom Solutions"):
-        link = '[Click here to Navigate](http://birdstack.tech/customsolutions.html)'
-        st.markdown(link, unsafe_allow_html=True)
+dropdown = st.sidebar.selectbox("Drop down to select",("Sales Order Processing","Accounting", "Payroll", "Employee Onboarding","Invoice Processing"))
+if dropdown == "Sales Order Processing":
+    if st.sidebar.checkbox("Get Analysis Report"):
+        st.success("Hello Decision Maker, here is your RPA report")
+        st.image("DMimages/salesorder.png", width=700)
+        if st.button("Pay Now & Get Full Report"):
+            link = '[Click here to Navigate](https://rzp.io/l/0ne27uKX)'
+            st.markdown(link, unsafe_allow_html=True)
+        if st.button("Interested in Custom Solutions"):
+            link = '[Click here to Navigate](http://birdstack.tech/customsolutions.html)'
+            st.markdown(link, unsafe_allow_html=True)
+if dropdown == "Accounting":
+    if st.sidebar.checkbox("Get Analysis Report"):
+        st.success("Hello Decision Maker, here is your RPA report")
+        st.image("DMimages/Accounting.png", width=700)
+        if st.button("Pay Now & Get Full Report"):
+            link = '[Click here to Navigate](https://rzp.io/l/0ne27uKX)'
+            st.markdown(link, unsafe_allow_html=True)
+        if st.button("Interested in Custom Solutions"):
+            link = '[Click here to Navigate](http://birdstack.tech/customsolutions.html)'
+            st.markdown(link, unsafe_allow_html=True)
+if dropdown == "Payroll":
+    if st.sidebar.checkbox("Get Analysis Report"):
+        st.success("Hello Decision Maker, here is your RPA report")
+        st.image("DMimages/payroll.png", width=700)
+        if st.button("Pay Now & Get Full Report"):
+            link = '[Click here to Navigate](https://rzp.io/l/0ne27uKX)'
+            st.markdown(link, unsafe_allow_html=True)
+        if st.button("Interested in Custom Solutions"):
+            link = '[Click here to Navigate](http://birdstack.tech/customsolutions.html)'
+            st.markdown(link, unsafe_allow_html=True)
+if dropdown == "Employee Onboarding":
+    if st.sidebar.checkbox("Get Analysis Report"):
+        st.success("Hello Decision Maker, here is your RPA report")
+        st.image("DMimages/Onboarding.png", width=700)
+        if st.button("Pay Now & Get Full Report"):
+            link = '[Click here to Navigate](https://rzp.io/l/0ne27uKX)'
+            st.markdown(link, unsafe_allow_html=True)
+        if st.button("Interested in Custom Solutions"):
+            link = '[Click here to Navigate](http://birdstack.tech/customsolutions.html)'
+            st.markdown(link, unsafe_allow_html=True)
+if dropdown == "Invoice Processing":
+    if st.sidebar.checkbox("Get Analysis Report"):
+        st.success("Hello Decision Maker, here is your RPA report")
+        st.image("DMimages/invoice.png", width=700)
+        if st.button("Pay Now & Get Full Report"):
+            link = '[Click here to Navigate](https://rzp.io/l/0ne27uKX)'
+            st.markdown(link, unsafe_allow_html=True)
+        if st.button("Interested in Custom Solutions"):
+            link = '[Click here to Navigate](http://birdstack.tech/customsolutions.html)'
+            st.markdown(link, unsafe_allow_html=True)
